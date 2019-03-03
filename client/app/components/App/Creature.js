6000c806eef70ba0c3c11c177bc38228515b8a3b
Abaddon16 <logan.gombar@gmail.com>
2019-03-02 18:59:09 -0600

    Model update & routing update
    - Made a creature model actually hold Laruna data
    - Updated the routing for creating a creature to use req.body to allow for *all* of the data to be pushed, not just the name via the URL

--- client/app/components/App/Creature.js
+++ client/app/components/App/Creature.js
@@ -2,7 +2,7 @@
  * Authored by: Abaddon16
  * Document Function: Main app component, takes in children componenets and they insert themselves as components
  */
-import events from 'events';
+
 import React, {Component} from 'react';
 import CreatureInput from './CreatureInput_comp';
 
@@ -17,7 +17,16 @@ class Creature extends Component {
 		this.updateCreatureList()
 	}
 	newCreature(name) {
-		fetch(`/api/creatures/${name}/`, {method: 'POST'})
+		fetch(`/api/creatures/${name}/`,
+			{
+				method: 'POST',
+				body: JSON.stringify(//! needs to be defined the same as the `/models/Creatures.js` model
+					{// TODO: Creatures comp - Find a way to use the Mongoose model here instead of having to remodel it over again here
+						name: name,
+						type: name
+					}),
+				headers: {"Content-Type": "application/json"}
+			})
 			.then(res => res.json())
 		this.updateCreatureList();
 	}
